49137d1462202a6ea640f7bb94e36da9c3f7eee5
Eltan-Tasli <eltan95140@gmail.com>
2021-12-01 16:36:07 +0100

    Partie 2 - Création d’un Logger

---
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -0,0 +1,10 @@
+package fr.lernejo.logger;
+
+public class LoggerFactory {
+
+    public static Logger getLogger(String name) {
+        Logger log = new ConsoleLogger();
+        return log;
+
+    }
+}
